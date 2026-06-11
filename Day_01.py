6db451262a80d1969ded45e39b2ef9a7279eb91d
Karan Dharwal <karandharwal.sd@gmai.com>
2026-06-11 20:58:10 +0530

    Done

--- Day_01.py
+++ Day_01.py
@@ -5,3 +5,20 @@ name = "Karan Dharwal"
 age = 20
 course = "Python Fundamentals"
 print(f"My name is {name}, I am {age} years old and I am learning {course}.")
+
+# Question 2: create a variable current_age and calculate and print your age after 5 years.
+current_age = 20
+age_after_5_years = current_age + 5
+print(f"In 5 years, I will be {age_after_5_years} years old.")
+
+# Question 3: take two integer and perform addition, subtraction, multiplication and division on them and print the results.
+num1 = 10
+num2 = 5
+addition = num1 + num2
+subraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f"Addition: {addition}")
+print(f"Subtraction: {subraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
