f5662aee177038a8e5322d6f97ca3d54280b0853
Karan Dharwal <karandharwal.sd@gmai.com>
2026-06-11 20:43:06 +0530

    done

--- Day_01.py
+++ Day_01.py
@@ -1,1 +1,7 @@
 print("Hello, Python!")
+
+# Q1 Write a Python program that: Stores your name Stores your age Stores your course and prints them in a sentence.
+name = "Karan Dharwal"
+age = 20
+course = "Python Fundamentals"
+print(f"My name is {name}, I am {age} years old and I am learning {course}.")
